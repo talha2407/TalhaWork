ca0039b46be720acd360a4e3ccc9f43ae79e8c8a
Ahmed Yaqoob <64608975+ahmedyaqoob99@users.noreply.github.com>
2025-10-23 01:58:41 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Answer # 3 //
+
+  int english = 78;
+  int urdu = 45;
+  int mathematics = 62;
+  int totalMarks = english + urdu + mathematics;
+  00;
+  num percentage = (totalMarks / 300) * 100;
+  print(
+      "Robert,\n English => $english,\n Urdu => $urdu, \n Mathematics => $mathematics \n, Total Marks => $totalMarks, \n Percentage => $percentage ");
+}
